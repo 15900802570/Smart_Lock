0ca2ea4b4ad329841f76bf81fc5a3a25fa27a50e
Jolly <wjl_0017@163.com>
2019-04-12 12:34:50 +0800

    ADD 新增屏幕适配文件

--- app/src/main/java/com/smart/lock/MainActivity.java
+++ app/src/main/java/com/smart/lock/MainActivity.java
@@ -62,6 +62,11 @@ public class MainActivity extends AppCompatActivity implements
         initView();
         initDate();
         initEvent();
+
+        int  height = this.getResources().getDisplayMetrics().heightPixels;
+        int width = this.getResources().getDisplayMetrics().widthPixels;
+        LogUtil.d(TAG, "height = "+ height+'\n'+
+                "width = "+width);
     }
 
     private void initView() {
